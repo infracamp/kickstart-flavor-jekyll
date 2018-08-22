8cf4d5ddc99f442bc52aa3bda0dd132e33748bbe
Matthias <m@tth.es>
2018-08-22 19:41:13 +0200

    added errordocuments

--- flavor/flavor-build.sh
+++ flavor/flavor-build.sh
@@ -14,6 +14,10 @@ gem install jekyll jekyll-optional-front-matter
 
 cd /kickstart/flavor && bundle install
 
+echo "ErrorDocument 404 /errors/404.html" >> /etc/apache/
+echo "ErrorDocument 500 /errors/500.html" >> /etc/apache/
+
+
 chown -R user /var/www
 rm -f /var/www/html/*
 
